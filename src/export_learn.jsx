978660598faff10508f3a11a254c54900a723494
abishekak18 <143478872+abishekak18@users.noreply.github.com>
2024-11-07 10:13:00 +0530

    Add files via upload

---
+++ src/export_learn.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Message = () => {
+    const name = "Mahi";
+    const age = 45;
+    return <div>{name} is {age} years old !!!</div>;
+}
+
+export default Message;

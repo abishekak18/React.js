60a522502a53ae662844527bc3eaf6d2ea13cb66
abishekak18 <143478872+abishekak18@users.noreply.github.com>
2024-11-09 13:16:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Message from './export_learn.jsx';
+import Garage from './props.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Message />
+    <Garage/>
   </StrictMode>,
 );

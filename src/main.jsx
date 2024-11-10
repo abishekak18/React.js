4e16d093acf5b188235e085d9405795b42854946
abishekak18 <143478872+abishekak18@users.noreply.github.com>
2024-11-10 09:33:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import Message from './export_learn.jsx';
 import Garage from './props.jsx';
+import Football from './events.jsx';
+import List from './list.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Message />
     <Garage/>
+    <Football/>
+    <List/>
   </StrictMode>,
 );
